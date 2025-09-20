65d3598afb341163e997f969b264871acdf55c97
abesoft4 <33007651+abesoft4@users.noreply.github.com>
2025-09-20 23:57:23 +0000

    Initialize main.c with student management TODOs
    Added initial structure and TODOs for student management.

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+// TODO: Define Student struct
+
+// TODO: Implement initStudent()
+
+// TODO: Implement printStudent()
+
+// TODO: Implement updateGPA()
+
+int main() {
+    // TODO: Create array of 3 Students
+
+    // TODO: Initialize them with initStudent()
+
+    // TODO: Print them
+
+    // TODO: Update GPA of one student and print again
+
+    // TODO: Use pointer to traverse array and print all students
+
+    return 0;
+}
